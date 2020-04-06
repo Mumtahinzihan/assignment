d41d23995c8ec1fc006c2090926832c1b30e866d
Mumtahinzihan <mumtahin@yahoo.com>
2020-04-06 07:30:22 +0100

    button deleted

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 import './Details.css';
-import Complete from '../Complete/Complete';
-import { Redirect } from "react-router-dom";
-
 
 export default class DetailsComponent extends React.Component {
 
@@ -28,63 +25,59 @@ export default class DetailsComponent extends React.Component {
         const assign = this.state.assignmentsGet;
         const group = this.state.array;
 
-        if (this.props.match.params.id === undefined) {
-            return <Redirect to='/not-found' />
-        } else {
-            return (
-                <div>
-                    <div><h1>Details of Assignment</h1></div>
+        return (
+            <div>
+                <div><h1>Details of Assignment</h1></div>
 
-                    <div className='detail'>
-                        <h2>Title: {assignment.title}</h2>
+                <div className='detail'>
+                    <h2>Title: {assignment.title}</h2>
 
-                        <div className='assignment'>
-                            <p>Instruction: {assignment.details}</p>
-                            <p>Links: {assignment.links}</p>
-                            <p>Assignment Id: {assignment.id}</p>
-                            <p>Suggested timescale: {assignment.suggestedTimescale}</p>
-                            <p>Deadline: {assignment.deadline}</p>
-                            <p>Set on: {assignment.setOn}</p>
-                            <Complete />
-                            <p>Complete: {assignment.isComplete ? 'Completed' : 'Not completed'}</p>
-                        </div>
-
-                        <div className='setby'>
-                            <h3>Set by:</h3>
-                            <p>Id: {assign.simsId}</p>
-                            <p>User type: {assign.userType}</p>
-                            <p>Email: {assign.email}</p>
-                            <p>Date of birth: {assign.dateOfBirth}</p>
-                            <p>Title: {assign.title}</p>
-                            <p>Id: {assign.id}</p>
-                            <p>First name: {assign.firstName}</p>
-                            <p>Initials: {assign.initials}</p>
-                            <p>Last name: {assign.lastName}</p>
-                        </div>
-
-                        <div className='group'>
-                            <h3>Groups:</h3>
-                            {
-                                group.map(gp => {
-                                    return (
-                                        <div key={gp.id}>
-                                            <p>Group id: {gp.id}</p>
-                                            <p>Name: {gp.name}</p>
-                                            <p>Friendly name: {gp.friendlyName}</p>
-                                            <p>School Id: {gp.school.id}</p>
-                                            <p>School name: {gp.school.name}</p>
-                                            <p>School code: {gp.school.code}</p>
-                                            <p>Group's code: {gp.code}</p>
-                                            <p>Group's type: {gp.type}</p>
-                                            <p>Group's summary: {gp.summary}</p>
-                                        </div>)
-                                })
-                            }
-                        </div>
-
+                    <div className='assignment'>
+                        <p>Instruction: {assignment.details}</p>
+                        <p>Links: {assignment.links}</p>
+                        <p>Assignment Id: {assignment.id}</p>
+                        <p>Suggested timescale: {assignment.suggestedTimescale}</p>
+                        <p>Deadline: {assignment.deadline}</p>
+                        <p>Set on: {assignment.setOn}</p>
+                        <p>Complete: {assignment.isComplete ? 'Completed' : 'Not completed'}</p>
                     </div>
+
+                    <div className='setby'>
+                        <h3>Set by:</h3>
+                        <p>Id: {assign.simsId}</p>
+                        <p>User type: {assign.userType}</p>
+                        <p>Email: {assign.email}</p>
+                        <p>Date of birth: {assign.dateOfBirth}</p>
+                        <p>Title: {assign.title}</p>
+                        <p>Id: {assign.id}</p>
+                        <p>First name: {assign.firstName}</p>
+                        <p>Initials: {assign.initials}</p>
+                        <p>Last name: {assign.lastName}</p>
+                    </div>
+
+                    <div className='group'>
+                        <h3>Groups:</h3>
+                        {
+                            group.map(gp => {
+                                return (
+                                    <div key={gp.id}>
+                                        <p>Group id: {gp.id}</p>
+                                        <p>Name: {gp.name}</p>
+                                        <p>Friendly name: {gp.friendlyName}</p>
+                                        <p>School Id: {gp.school.id}</p>
+                                        <p>School name: {gp.school.name}</p>
+                                        <p>School code: {gp.school.code}</p>
+                                        <p>Group's code: {gp.code}</p>
+                                        <p>Group's type: {gp.type}</p>
+                                        <p>Group's summary: {gp.summary}</p>
+                                    </div>)
+                            })
+                        }
+                    </div>
+
                 </div>
-            )
-        }
+            </div>
+        )
+
     }
 }
